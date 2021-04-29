f3352429c7fa7348ab80eb0d386dbacfb9e7cb4c
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-04-30 00:57:14 +0800

    feat: filtering the list of movie

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,50 @@
+import { useState } from "react";
 import AuthorIntro from "components/AuthorIntro";
 import CardItem from "components/CardItem";
 import CardListItem from "components/CardListItem";
+import FilteringMenu from "components/FilteringMenu";
 import PageLayout from "components/PageLayout";
 import { getAllMovies } from "lib/api";
 import { Row, Col } from "react-bootstrap";
 
 export default function Home({ movie }) {
+  const [filter, setFilter] = useState({
+    view: { list: 0 },
+  });
   return (
     <PageLayout>
       <div className="movie-detail-page">
-        <Row>
-          <Col md="8">
-            <AuthorIntro />
-          </Col>
-        </Row>
+        <AuthorIntro />
+        <FilteringMenu
+          filter={filter}
+          onChange={(option, value) => {
+            setFilter({ ...filter, [option]: value });
+          }}
+        />
         <hr />
         <Row className="mb-5">
-          <Col md="10">
-            <CardListItem />
-          </Col>
-          {movie.map((movie) => (
-            <Col key={movie.slug} md="4">
-              <CardItem
-                title={movie.title}
-                popularity={movie.popularity}
-                image={movie.poster}
-                date={movie.releaseDate}
-                director={movie.director}
-                overview={movie.overview}
-                link={{
-                  href: "/movie/[slug]",
-                  as: `/movie/${movie.slug}`,
-                }}
-              />
-            </Col>
-          ))}
+          {movie.map((movie) =>
+            !filter.view.list ? (
+              <Col key={movie.slug} md="4">
+                <CardItem
+                  title={movie.title}
+                  popularity={movie.popularity}
+                  image={movie.poster}
+                  date={movie.releaseDate}
+                  director={movie.director}
+                  overview={movie.overview}
+                  link={{
+                    href: "/movie/[slug]",
+                    as: `/movie/${movie.slug}`,
+                  }}
+                />
+              </Col>
+            ) : (
+              <Col md="10">
+                <CardListItem />
+              </Col>
+            )
+          )}
         </Row>
       </div>
     </PageLayout>
