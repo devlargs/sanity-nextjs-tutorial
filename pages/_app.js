59f91e887e2786773142f65c403ab8b7a59b92c1
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-04-30 00:55:30 +0800

    feat: import fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { faBorderAll, faList } from "@fortawesome/free-solid-svg-icons";
+
+config.autoAddCss = false;
+library.add(faBorderAll, faList);
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "styles/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "highlight.js/styles/vs2015.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
+export default ({ Component, pageProps }) => <Component {...pageProps} />;
