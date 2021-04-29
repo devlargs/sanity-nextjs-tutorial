b546b25d7e17d4f94aecf2a11683342d66c54582
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-04-29 18:47:45 +0800

    feat: import HighlightCode

--- components/MovieContent.js
+++ components/MovieContent.js
@@ -1,12 +1,21 @@
 import BlockContent from "@sanity/block-content-to-react";
+import { urlFor } from "lib/api";
+import HighlightCode from "./HighlightCode";
 
 const serializer = {
   types: {
-    code: ({ node: { language, code } }) => (
-      <pre data-language={language}>
-        <code>{code}</code>
-      </pre>
-    ),
+    code: ({ node: { language, code } }) => {
+      return <HighlightCode language={language}>{code}</HighlightCode>;
+    },
+    image: ({ node: { asset } }) => {
+      return (
+        <div className="movie-image">
+          <div class="movie-image">
+            <img src={urlFor(asset).height(300).fit("max")} />
+          </div>
+        </div>
+      );
+    },
   },
 };
 
