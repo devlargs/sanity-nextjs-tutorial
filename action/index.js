6eb3d16a766d3522e7fbb18b7a920f4f8f372400
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-05-11 08:06:13 +0800

    feat: set offset to 0 instead of null

--- action/index.js
+++ action/index.js
@@ -2,5 +2,5 @@ import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export const useGetMovies = (initialData) =>
-  useSWR(`/api/movies`, fetcher, { initialData });
+export const useGetMovies = ({ offset }, initialData) =>
+  useSWR(`/api/movies?offset=${offset || 0}`, fetcher, { initialData });
