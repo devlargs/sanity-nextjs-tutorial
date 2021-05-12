b516d216711383dd3598c9ed71507996b4941c4f
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-05-12 13:23:18 +0800

    import getPaginatedMovies

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,8 +1,8 @@
-import { getAllMovies } from "lib/api";
+import { getPaginatedMovies } from "lib/api";
 
 export default async function getMovies(req, res) {
   const offset = parseInt(req.query.offset || 0, 10);
   const releaseDate = req.query.releaseDate || "desc";
-  const data = await getAllMovies({ offset, releaseDate });
+  const data = await getPaginatedMovies({ offset, releaseDate });
   res.status(200).json(data);
 }
